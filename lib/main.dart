5959d20ff7b73f745f0b644da357ec1c5ddb4fd9
CodingSmiles <aadirajanil726@gmail.com>
2024-01-21 18:47:17 +0530

    visuals

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: const HomePage(),
       title: "Testing",
-      theme: ThemeData(),
+      theme: ThemeData(
+
+        colorSchemeSeed: Palette.calmBlue,
+        useMaterial3: true,
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -27,10 +31,115 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CAppBar(
+    return Scaffold(
+      backgroundColor: Palette.calmBlue,
+      appBar: const CAppBar(
         title: "AI Mastery",
         centerTitle: true,
+      ),
+      body: LoginPage(
+        key: widget.key,
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 12.0,
+              right: 12.0,
+              top: 12.0,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Enter a value first";
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text(
+                        "User ID",
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Processing Data'),
+                          ),
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Palette.offWhite),
+                    child: const Text('Submit'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool centerTitle;
+  final PreferredSizeWidget? bottom;
+
+  const CAppBar(
+      {super.key, required this.title, required this.centerTitle, this.bottom});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      bottom: bottom,
+      scrolledUnderElevation: 4.0,
+      backgroundColor: Palette.calmBlue,
+      centerTitle: centerTitle,
+      actions: <Widget>[
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.more_vert,
+            semanticLabel: 'More Options',
+          ),
+        )
+      ],
+      title: Text(
+        title,
+        textScaleFactor: ScaleSize.textScaleFactor(context),
+        style: const TextStyle(
+          fontSize: 28,
+        ),
       ),
     );
   }
@@ -46,28 +155,9 @@ class ScaleSize {
   }
 }
 
-class CAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final String title;
-  final bool centerTitle;
-
-  const CAppBar({super.key, required this.title, required this.centerTitle});
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      centerTitle: centerTitle,
-      title: Text(
-        title,
-        textScaleFactor: ScaleSize.textScaleFactor(context),
-        style: const TextStyle(
-          fontSize: 28,
-          fontFamily: 'Inter',
-          letterSpacing: 2,
-        ),
-      ),
-    );
-  }
+class Palette {
+  static const Color calmBlue = Color.fromRGBO(47, 156, 149, 1);
+  static const Color lightBlue = Color.fromRGBO(64, 201, 162, 1);
+  static const Color darkBlue = Color.fromRGBO(10, 96, 142, 1);
+  static const Color offWhite = Color.fromRGBO(255, 251, 244, 1);
 }
